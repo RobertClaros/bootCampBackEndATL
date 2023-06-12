fd96a62f1667ecf2dfa53d952eac8921531ac197
“Robert <audioprobolivia@gmail.com>
2023-06-12 18:08:16 -0400

    Se agrego la clase 7 del 12 de Junio

--- Clase7_ATL_12_06/src/NumerosVector.java
+++ Clase7_ATL_12_06/src/NumerosVector.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class NumerosVectorSeparado {
+public class NumerosVector {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
